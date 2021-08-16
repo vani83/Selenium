8efe2f886c6be2097bda47a9ae24557dac5c1252
vani83 <vaniyalamanchi@gmail.com>
2021-08-16 20:29:16 +0800

    Added Class2 to Project1

---
+++ Project1/src/com/Pack1/Class2.java
@@ -0,0 +1,11 @@
+package com.Pack1;
+
+public class Class2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is first line....");
+
+	}
+
+}
--- Seleniumjuly/src/com/Launching/Class1.java
+++
@@ -1,9 +0,0 @@
-package com.Launching;
-
-public class Class1 
-{
-public static void main(String[] args)
-{
-	System.out.println("This is first line....");
-}
-}
