c48c8d73dd37ace04b56ebbcee95ba1fd2c2016b
vani83 <vaniyalamanchi@gmail.com>
2021-08-16 20:33:09 +0800

    Added 2nd line in Class2

--- Project1/src/com/Pack1/Class2.java
+++ Project1/src/com/Pack1/Class2.java
@@ -5,7 +5,7 @@ public class Class2 {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is first line....");
-
+        System.out.println("This is Second line.....");
 	}
 
 }
